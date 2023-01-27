5504e7a2a5ad95d0884b0a9e5b7f675f33706a08
sanghyunEE <wxx7829@naver.com>
2023-01-28 05:53:31 +0900

    #2606 설명 추가

--- dfs&bfs/2606.py
+++ dfs&bfs/2606.py
@@ -12,6 +12,8 @@ visited = [False] * (n + 1)
 
 
 result = [0] # global 선언 할지 vs Argument로 넘길지
+# 참고 : https://chanos.tistory.com/m/entry/%EB%B0%B1%EC%A4%80-2606%EB%B2%88-%EB%B0%94%EC%9D%B4%EB%9F%AC%EC%8A%A4-%ED%8C%8C%EC%9D%B4%EC%8D%AC-%EB%AC%B8%EC%A0%9C-%ED%92%80%EC%9D%B4-DFS%EC%99%80-BFS-%EC%B0%A8%EC%9D%B4
+# 또는 visited가 True인 것을 sum 하면 된다
 
 def dfs(x):
     visited[x] = True
