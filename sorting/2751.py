774e53da7c418e20a5d5a5d766ea2ff7141b3c6b
sanghyunEE <wxx7829@naver.com>
2023-02-02 11:25:37 +0900

    #2751 설명 추가

--- sorting/2751.py
+++ sorting/2751.py
@@ -1,4 +1,6 @@
-n = int(input())
+import sys
+
+n = int(sys.stdin.readline())
 
 array = []
 for _ in range(n):
@@ -7,3 +9,5 @@ array.sort()
 
 for i in array:
     print(i)
+
+# https://coarmok.tistory.com/entry/%ED%8C%8C%EC%9D%B4%EC%8D%ACpython-%EB%B0%B1%EC%A4%80-2751%EB%B2%88-merge-sort
