68598d936c334e069e4dabefbeaabaa26dfad37a
sanghyunEE <wxx7829@naver.com>
2023-02-12 15:54:53 +0900

    #2468 unsolved

---
+++ dfs&bfs/2468.py
@@ -0,0 +1,35 @@
+from sys import stdin, setrecursionlimit
+from copy import deepcopy
+setrecursionlimit(10**6)
+
+n = int(input())
+array = []
+for _ in range(n):
+    array.append(list(map(int, stdin.readline().split())))
+
+def dfs(arr, x, y, t):
+    if x < 0 or x >= n or y < 0 or y >= n:
+        return False
+    if arr[x][y] <= t or arr[x][y] == 0:
+        return False
+    arr[x][y] = 0
+    dfs(arr, x-1, y, t)
+    dfs(arr, x+1, y, t)
+    dfs(arr, x, y-1, t)
+    dfs(arr, x, y+1, t)
+    return True
+
+min_val = min(map(min, array))
+max_val = max(map(max, array))
+
+current = -1
+for t in range(min_val, max_val + 1):
+    region = 0
+    arr = deepcopy(array) # deep copy 써라
+    for i in range(n):
+        for j in range(n):
+            if dfs(arr, i, j, t):
+                region += 1
+    current = max(current, region)
+
+print(current)
