1a04c94f5be9b5a45f72c94b37017087a7592f37
sanghyunEE <wxx7829@naver.com>
2023-04-09 22:15:32 +0900

    #15649 nm 시리즈 1

--- backtracking/15649.py
+++ backtracking/15649.py
@@ -1,17 +1,23 @@
+# https://velog.io/@yusuk6185/%EB%B0%B1%EC%A4%80-15649-N%EA%B3%BC-M-1-%ED%8C%8C%EC%9D%B4%EC%8D%AC-with-%EB%B0%B1%ED%8A%B8%EB%9E%98%ED%82%B9
+# https://www.youtube.com/watch?v=atTzqxbt4DM
+
+
 n, m = map(int, input().split())
 
-data = [x for x in range(1, n + 1)]
+def dfs():
+    if len(s) == m:
+        print(' '.join(map(str, s)))
+        return
+    for i in range(1, n + 1):
+        if visited[i]:
+            continue
+        visited[i] = True
+        s.append(i)
+        dfs()
+        s.pop()
+        visited[i] = False
 
-from itertools import permutations
+s = []
+visited = [False] * (n + 1)
 
-result = list(permutations(data, m))
-
-for i in result:
-    for j in i:
-        print(j, end=' ')
-    print()
-
-
-# documetation 보고 풀었음\
-
-# 근데 백트래킹으로 풀어야지 ㅇㅇ 순열 문제는
+dfs()
