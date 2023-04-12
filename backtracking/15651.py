2f5bf102611edcd70baa5527c383a9c377126869
sanghyunEE <wxx7829@naver.com>
2023-04-12 10:31:43 +0900

    #15651 nm시리즈 3

---
+++ backtracking/15651.py
@@ -0,0 +1,15 @@
+
+n, m = map(int, input().split())
+
+s = []
+
+def go():
+    if len(s) == m:
+        print(' '.join(map(str, s)))
+        return
+    for i in range(1, n + 1):
+        s.append(i)
+        go()
+        s.pop()
+
+go()
