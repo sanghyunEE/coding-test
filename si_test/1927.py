b95a7fcdf7de8a084a2056be9e9ff39f2a5900f0
sanghyunEE <wxx7829@naver.com>
2023-04-29 21:36:30 +0900

    #1927 solved

---
+++ si_test/1927.py
@@ -0,0 +1,16 @@
+import heapq, sys
+
+input = sys.stdin.readline
+
+q = []
+
+n = int(input())
+for _ in range(n):
+    x = int(input())
+    if x == 0:
+        if q:
+            print(heapq.heappop(q))
+        else:
+            print(0)
+    else:
+        heapq.heappush(q, x)
